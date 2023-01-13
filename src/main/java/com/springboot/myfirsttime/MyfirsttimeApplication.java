59ab05430a288953d265cade76b1c0904f667121
gangwoo <khyme1022@naver.com>
2023-01-13 16:42:14 +0900

    기본 세팅 커밋

---
+++ src/main/java/com/springboot/myfirsttime/MyfirsttimeApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.myfirsttime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirsttimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirsttimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/myfirsttime/MyfirsttimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.myfirsttime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirsttimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
