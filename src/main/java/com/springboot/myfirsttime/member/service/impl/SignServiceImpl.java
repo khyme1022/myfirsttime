96ef65ac7c7a9f97fd4b2f9798acce84e48b6a94
gangwoo <khyme1022@naver.com>
2023-02-16 19:06:08 +0900

    SignServiceImpl 소폭 수정

--- src/main/java/com/springboot/myfirsttime/member/service/impl/SignServiceImpl.java
+++ src/main/java/com/springboot/myfirsttime/member/service/impl/SignServiceImpl.java
@@ -34,7 +34,7 @@ public class SignServiceImpl implements SignService {
     // 회원가입 처리 메소드
     @Override
     public SignUpResultDto signUp(String id, String password, String name, String role, Gender gender) {
-        LOGGER.info("[getSignUpResult] 회원가입 정보 전달");
+        LOGGER.info("[SignUp] 회원가입 정보 전달");
         User user;
         if(role.equalsIgnoreCase("admin")){ // 권한이 관리자일 경우 회원가입
             user = User.builder()
@@ -55,14 +55,14 @@ public class SignServiceImpl implements SignService {
         }
 
         User savedUser = userRepository.save(user);
-        SignUpResultDto signUpResultDto = new SignInResultDto();
+        SignUpResultDto signUpResultDto = new SignUpResultDto();
 
-        LOGGER.info("[getSignUpResult] userEntity 값이 들어왔는지 확인 후 결과값 주입");
+        LOGGER.info("[SignUp] userEntity 값이 들어왔는지 확인 후 결과값 주입");
         if(!savedUser.getName().isEmpty()){
-            LOGGER.info("[getSignUpResult] 정상처리 완료");
+            LOGGER.info("[SignUp] 정상처리 완료");
             setSuccessResult(signUpResultDto);
         }else{
-            LOGGER.info("[getSignUpResult] 실패");
+            LOGGER.info("[SignUp] 실패");
             setFailResult(signUpResultDto);
         }
         return signUpResultDto;
@@ -70,22 +70,22 @@ public class SignServiceImpl implements SignService {
 
     @Override
     public SignInResultDto signIn(String id, String password) throws RuntimeException {
-        LOGGER.info("[getSignInResult] 회원 정보 요청");
+        LOGGER.info("[SignIn] 회원 정보 요청");
         User user = userRepository.getByUid(id);
-        LOGGER.info("[getSignInResult] ID : {}", user.getId());
+        LOGGER.info("[SignIn] ID : {}", user.getId());
         
         LOGGER.info("패스워드 비교 수행");
         if(!passwordEncoder.matches(password, user.getPassword())){
             throw new RuntimeException();
 
         }
-        LOGGER.info("[getSignInResult] 패스워드 일치");
+        LOGGER.info("[SignIn] 패스워드 일치");
 
-        LOGGER.info("[getSignInResult] SignInResultDto 객체 생성");
+        LOGGER.info("[SignIn] SignInResultDto 객체 생성");
         SignInResultDto signInResultDto = SignInResultDto.builder()
                 .token(jwtTokenProvider.createToken(String.valueOf(user.getUid()),user.getRoles()))
                 .build();
-        LOGGER.info("[getSignInResult] 객체에 값 주입");
+        LOGGER.info("[SignIn] 객체에 값 주입");
         setSuccessResult(signInResultDto);
 
         return signInResultDto;
